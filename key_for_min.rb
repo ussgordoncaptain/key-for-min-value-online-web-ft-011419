682134b976a0b32cd9e3df44f32a16e05b87d603
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-20 04:35:48 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,26 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  keys_arr = []
+  values_arr = []
+  name_hash.map do |key, value|
+    keys_arr << key
+    values_arr << value 
+  end
+  if keys_arr.size == 0 
+    return nil 
+  end
+  min_key = keys_arr[0]
+  min_value = values_arr[0]
+  ii=1 
+  while (ii < keys_arr.size)
+    if (values_arr[ii]<min_value)
+      min_value=values_arr[ii]
+      min_key = keys_arr[ii]
+      
+    end
+    ii+=1
+  end
+  #so the issue is that there is no general way to call a single random key value pair from a hash? so I made 2 parallel vectors instead
+  return min_key
 end
